25dfed618c2fa4a9ef11d8b998a17f0636b16c0b
ElisandroPeixoto <elisandropeixoto21@gmail.com>
2026-03-09 15:32:54 -0300

    phase_components method

--- src/core/symmetrical_components.py
+++ src/core/symmetrical_components.py
@@ -24,3 +24,18 @@ class SymmetricalComponents:
             'x1': rect_to_polar(x1),
             'x2': rect_to_polar(x2),
         }
+
+    @staticmethod
+    def phase_components(x0: tuple, x1: tuple, x2: tuple):
+        # Convert tuples into complex numbers
+        c0, c1, c2 = (rect(r, radians(phi)) for r, phi in (x0, x1, x2))
+
+        xa = c0 + c1 + c2
+        xb = c0 + alpha**2*c1 + alpha*c2
+        xc = c0 + alpha*c1 + alpha**2*c2
+
+        return {
+            'xa': rect_to_polar(xa),
+            'xb': rect_to_polar(xb),
+            'xc': rect_to_polar(xc),
+        }
